c8af20507ec193c3c6b58b88d50b2d5a59c22d39
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2017-02-12 22:28:24 +0200

    shell: optimize bin/cops_docker_cleanup.sh

--- bin/cops_docker_cleanup.sh
+++ bin/cops_docker_cleanup.sh
@@ -1,33 +1,44 @@
 #!/usr/bin/env bash
 # try to cleanup most of the dangling containers and images
-while read i;do
-    docker rm -f $i
-done < <( \
-    docker ps -a|grep  Exited|grep "sh -c 'echo" |awk '{print $1}' )
 
-while read i;do
-    docker rm -f $i
-done < <( \
-    docker ps -a|grep  Exited|grep "sh -c '#(nop)" |awk '{print $1}' )
+DATA_FILTER="mysql|postgresql|pgsql|redis|mongodb|storage|backup"
 
-while read i;do
-    docker rm -f $i
-done < <( \
-    docker ps -a|grep  Exited|grep "sh -c 'step_rev" |awk '{print $1}' )
+# remove intermediates containers
+# and retain data containers
+cids="
+$( \
+    docker ps -a --no-trunc \
+    | grep  Exited \
+    | egrep "sh -c '(echo|#\(nop|.*step_rev.*)" \
+    | awk '{print $1}' )
+$( \
+    docker ps -a --no-trunc \
+    | grep  Exited \
+    | grep '"bash"'\
+    | egrep -v "$DATA_FILTER"\
+    | awk '{print $1}' )
+"
 
-while read i; do
-    docker rm -f $i
-done < <( \
-        docker ps -a|grep  Exited|grep '"bash"'\
-        | egrep -v "mysql|postgresql|pgsql|redis|mongodb|storage|backup"\
-        | awk '{print $1}' )
-
-while read i;do
-    docker rmi $i
-done < <( \
+# remove old images
+# remove cops test images
+imgids="
+$( \
     docker images --filter dangling=true -q )
-while read i;do
-    docker rmi $i
-done < <( \
+$( \
     docker images -a|grep copstest|awk '{print $3}' )
+"
+
+while read i;do
+    if [[ -n "${i}" ]];then
+        echo "Remove container: $i" >&2
+        docker rm -f $i >/dev/null
+    fi
+done < <(echo ${cids}|xargs -n1|sort -u)
+
+while read i;do
+    if [[ -n "${i}" ]];then
+        echo "Remove image: $i" >&2
+        docker rmi $i >/dev/null
+    fi
+done < <(echo ${imgids}|xargs -n1|sort -u)
 # vim:set et sts=4 ts=4 tw=80:
