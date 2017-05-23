67162efea0bf9bfc0d8ea0f681a87c8190d96c13
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2017-05-23 11:44:06 +0200

    docker: define entry point

--- hacking/docker_systemdebug.sh
+++ hacking/docker_systemdebug.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 cd $(dirname $0)
 W=$(pwd)
+EP=/sbin/cops_entry_point.sh
 docker run --cap-add SYS_PTRACE --cap-add SYS_ADMIN -e IMG_DEBUG=1 \
      -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
-     -v $W/container_rootfs/sbin/cops_systemd_entry_point.sh:/sbin/cops_systemd_entry_point.sh \
-     corpusops/ubuntu:16.04 "/sbin/cops_systemd_entry_point.sh"
+     -v $W/container_rootfs/$EP:$EP \
+     corpusops/ubuntu:16.04 "$EP"
 # vim:set et sts=4 ts=4 tw=80:
