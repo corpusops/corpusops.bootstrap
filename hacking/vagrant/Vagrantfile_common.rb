c346e9c6f10d2e20652326c580266700a5e128de
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2017-11-14 23:21:14 +0100

    vagrant: ensure refreshed facts on fresh provisioning

--- hacking/vagrant/Vagrantfile_common.rb
+++ hacking/vagrant/Vagrantfile_common.rb
@@ -379,10 +379,14 @@ def cops_init(opts)
     # extra provision, value in dictionnary can be either a file or a hash containing ansible variables
     cfg['PLAYBOOKS'] = {
         "default" => [
-            {"#{cfg['COPS_REL_PLAYBOOKS']}/provision/vagrant/net.yml" => {}},
-            {"#{cfg['COPS_REL_PLAYBOOKS']}/provision/vagrant/locales.yml" => {}},
-            {"#{cfg['COPS_REL_PLAYBOOKS']}/provision/vagrant/timezone.yml" => {}},
-            {"#{cfg['COPS_REL_PLAYBOOKS']}/provision/vagrant/sync_sshkeys.yml" => {}},
+            {"#{cfg['COPS_REL_PLAYBOOKS']}/provision/vagrant/net.yml" => {
+                :raw_arguments => ["--flush-cache"]}},
+            {"#{cfg['COPS_REL_PLAYBOOKS']}/provision/vagrant/locales.yml" => {
+                :raw_arguments => ["--flush-cache"]}},
+            {"#{cfg['COPS_REL_PLAYBOOKS']}/provision/vagrant/timezone.yml" => {
+                :raw_arguments => ["--flush-cache"]}},
+            {"#{cfg['COPS_REL_PLAYBOOKS']}/provision/vagrant/sync_sshkeys.yml" => {
+                :raw_arguments => ["--flush-cache"]}},
             {"#{cfg['COPS_REL_PLAYBOOKS']}/provision/vagrant/sshfs.yml" => {}},
             {"#{cfg['COPS_REL_PLAYBOOKS']}/provision/vagrant/cleanup.yml" => {}},
         ]
