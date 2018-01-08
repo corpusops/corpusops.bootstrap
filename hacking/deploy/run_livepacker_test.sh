f62edb4ce7487c5f3c508680c0727d76f6781725
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2018-01-08 01:45:14 +0100

    Refresh livepacker test script

---
+++ hacking/deploy/run_livepacker_test.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#
+# Wrappper to launch a container in test mode to debug packer builds
+# eg: ./bin/run_livepacker_test.sh .docker/packer/foobar.json
+#
+
+COPS_SCRIPTS_DIR="$(cd "$(dirname "$0")" && pwd)"
+if [ -e "$COPS_SCRIPTS_DIR/ansible_deploy_env" ];then
+    . "$COPS_SCRIPTS_DIR/ansible_deploy_env"
+fi
+
+"$LOCAL_COPS_ROOT/hacking/docker_livepacker_test.sh" $@
+# vim:set et sts=4 ts=4 tw=0:
