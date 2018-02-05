96a2edae65d8e701f2b0ec4aeb614bac3129c037
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2018-02-05 12:24:19 +0100

    install.sh & docker: Split build time only packages

--- hacking/container_rootfs/sbin/cops_container_strip.sh
+++ hacking/container_rootfs/sbin/cops_container_strip.sh
@@ -1,13 +1,22 @@
 #!/usr/bin/env bash
+SOFT_STRIP=y
+if echo ${container-}|grep -iq docker;then
+    SOFT_STRIP=
+fi
 GIT_SHALLOW_DEPTH=${GIT_SHALLOW_DEPTH}
 GIT_SHALLOW=${GIT_SHALLOW-}
-NO_IMAGE_STRIP=${NO_IMAGE_STRIP-y}
+NO_IMAGE_STRIP=${NO_IMAGE_STRIP-$SOFT_STRIP}
+NO_YUM_CLEANUP=${NO_YUM_CLEANUP-}
 NO_ANSIBLE_STRIP=${NO_ANSIBLE_STRIP-}
-NO_GCC_STRIP=${NO_GCC_STRIP-}
+NO_PYC_STRIP=${NO_PYC_STRIP-}
+NO_DOC_STRIP=${NO_DOC_STRIP-$SOFT_STRIP}
+NO_GCC_STRIP=${NO_GCC_STRIP-$SOFT_STRIP}
 NO_GIT_PACK=${NO_GIT_PACK-}
 NO_CLEANUP=${NO_CLEANUP-}
+NO_MAN_STRIP=${NO_MAN_STRIP-$SOFT_STRIP}
 NO_SNAPSHOT=${NO_SNAPSHOT-}
-DEB_REMOVES=${DEB_REMOVES-}
+PKGS_REMOVES=${PKGS_REMOVES-}
+DEV_AUTOREMOVE=${DEV_AUTOREMOVE-}
 COPS_ROOT=${COPS_ROOT:-/srv/corpusops/corpusops.bootstrap}
 
 vv() { echo "$@" >&2;"$@"; }
@@ -55,11 +64,79 @@ detect_os() {
     export DISTRIB_ID DISTRIB_CODENAME DISTRIB_RELEASE
 }
 detect_os
-if [[ -z $NO_GIT_PACK ]] && [[ -e $COPS_ROOT/bin/git_pack ]];then
-    vv $COPS_ROOT/bin/git_pack /
+MAN_DIRS=${MAN_DIRS:-"
+/usr/share/man
+/usr/local/share/man
+"}
+DOC_DIRS=${DOC_DIRS:-"
+/usr/share/info
+/usr/local/share/info
+/usr/share/doc
+/usr/local/share/doc
+"}
+DOC_STRIPS=${DOC_STRIPS:-"
+*.info.gz
+news*
+readme*
+author*
+license*
+copyright*
+deb*
+ubuntu*
+todo*
+changelog*
+changes*
+coding_style*
+distro_porting*
+differences*
+history*
+ack*
+*notes*
+thank*
+faq*
+bug*
+"}
+DOC_RM=${DOC_RM:-"
+ python git/RelNotes socat/socat.html"}
+if [[ -z $NO_DOC_STRIP ]];then
+    while read docdir;do
+        if [ -d "$docdir" ];then
+            for i in $DOC_RM;do
+                d="$docdir/$i"
+                if [ -e "$d" ];then rm -rfv $d;fi
+            done
+            while read sdocdir;do
+                for pattern in $DOC_STRIPS;do
+                    find $sdocdir -iname "$pattern" -delete -type f
+                done
+            done < <(find $docdir -maxdepth 1 -mindepth 1)
+        fi
+    done <<< "$DOC_DIRS"
+fi
+if [[ -z $NO_MAN_STRIP ]];then
+    while read mandir;do
+        if [ -d "$mandir" ];then
+            while read i;do
+                echo "Wiping manpage: $i";rm -f "$i"
+            done < <(find "$mandir" -type f)
+        fi
+    done <<< "$MAN_DIRS"
 fi
 if [[ -z $NO_CLEANUP ]] && [[ -e /sbin/cops_container_cleanup.sh ]];then
     vv /sbin/cops_container_cleanup.sh
+fi
+if [[ -z ${NO_PYC_STRIP} ]];then
+    find / -type f -name "*.pyc" -delete
+fi
+if [[ -z ${NO_YUM_CLEANUP} ]];then
+    if echo $DISTRIB_ID|egrep -iq "redhat|red-hat|ol|centos";then
+        vv rm -rf /usr/lib/firmware/*
+        for i in /var/cache/yum;do
+            if [ -e "$î" ];then
+                vv rm -rf "$i"
+            fi
+        done
+    fi
 fi
 if [[ -z $NO_SNAPSHOT ]] && [[ -e /sbin/cops_container_snapshot.sh ]];then
     vv /sbin/cops_container_snapshot.sh
@@ -73,10 +150,82 @@ if [[ -z $NO_IMAGE_STRIP ]];then
         for i in $aps;do
           if [ -e "$i" ];then vv rm -rf "$i";fi
         done
+        for corpusopsgit in \
+            $COPS_ROOT/roles/corpusops.roles/.git \
+            $COPS_ROOT/.git;do
+            if [ -e $corpusopsgit ];then
+                cd $corpusopsgit/..
+                git log|xz>git_log.gz
+                rm -rvf $corpusopsgit
+                cd ..
+            fi
+        done
     fi
-    if [[ -n "${DEB_REMOVES}" ]];then
-        if echo $DISTRIB_ID|egrep -iq "ubuntu|debian|mint";then
-            apt-get autoremove -y $DEB_REMOVES
+fi
+if [[ -z $NO_GIT_PACK ]] && [[ -e $COPS_ROOT/bin/git_pack ]];then
+    vv $COPS_ROOT/bin/git_pack /
+fi
+deb_filter_pkg_in_cache() {
+    local pkgs=
+    for i in $@;do
+        if apt-cache show $i >/dev/null 2>&1;then
+            pkgs="$pkgs $i"
+        fi
+    done
+    echo $pkgs
+}
+rh_filter_pkg_in_cache() {
+    local pkgs=
+    for i in $@;do
+        if yum info $i >/dev/null 2>&1;then
+            pkgs="$pkgs $i"
+        fi
+    done
+    echo $pkgs
+}
+if [[ -z $NO_IMAGE_STRIP ]];then
+    if echo $DISTRIB_ID|egrep -iq "redhat|red-hat|ol|centos";then
+        # remove dev pkgs
+        # while manually installing their lib counterpart
+        if [[ -n "${DEV_AUTOREMOVE}" ]];then
+            devpkgs=$(rpm -qa|egrep  -- "-devel|-header)")
+            pkgs=$(rpm -qa|egrep  -- "-devel|-header"\
+                   |sed -re "s/(-devel|-header).*//g")
+            for i in $(rh_filter_pkg_in_cache $pkgs);do
+                vv yum install -y $i || /bin/true
+            done
+            vv yum autoremove -y $(rh_filter_pkg_in_cache $devpkgs)
+        fi
+        # remove some unwanted pkgs
+        if [[ -n "${PKGS_REMOVES}" ]];then
+            vv yum autoremove -y  $(rh_filter_pkg_in_cache $PKGS_REMOVES)
+        fi
+    fi
+    if echo $DISTRIB_ID|egrep -iq "ubuntu|debian|mint";then
+        # remove dev pkgs
+        # while manually installing their lib counterpart
+        if [[ -n "${DEV_AUTOREMOVE}" ]];then
+            devpkgs=$(dpkg -l\
+                |grep ii|grep -- -dev\
+                |awk '{print $2}'\
+                |sed -re "s/:(amd64|i386)//g")
+            pkgs=""
+            for i in $devpkgs;do
+                pkgs="$pkgs ${pkg//-dev}"
+            done
+            for i in $(deb_filter_pkg_in_cache $pkgs);do
+                vv apt-get install \
+                    --ignore-missing \
+                    --no-install-recommends -y \
+                    $i || /bin/true
+            done
+            vv apt-get remove --auto-remove -y \
+                $(deb_filter_pkg_in_cache $devpkgs)
+        fi
+        # remove some unwanted pkgs
+        if [[ -n "${PKGS_REMOVES}" ]];then
+            vv apt-get autoremove -y \
+                $(deb_filter_pkg_in_cache $PKGS_REMOVES)
         fi
     fi
 fi
