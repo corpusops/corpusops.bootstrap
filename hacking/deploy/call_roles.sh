b92588e29a44c270e8c6d35ebc89bcaad692277a
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2018-04-10 09:19:25 +0200

    doc: call_roles.sh

--- hacking/deploy/call_roles.sh
+++ hacking/deploy/call_roles.sh
@@ -11,31 +11,30 @@ usage() {
 [PLAYBOOK= ] \\
 [QUIET=1 ]\\
 [NO_SILENT= ]\\
+[A_HOSTS=all] \\
 [A_INVENTORY=.ansible/inventory_foo ] \\
     '$0' [$@]
 
 Call ansible-playbook with all ansible variables and vaults (variables files) preseted.
-   - PLAYBOOK, vaults & A_INVENTORY are normally automatically setted up
+- PLAYBOOK, vaults & A_INVENTORY are normally automatically setted up
      depending on the A_ENV_NAME variable
--
-    PLAYBOOY=.ansible/playbooks/foo.yml \\
+- PLAYBOOY=.ansible/playbooks/foo.yml \\
         '$0'
     -> Will append automatically corpusops related vars based on \$A_ENV_NAME
--
-    '$0' .ansible/playbooks/foo.yml
+- '$0' .ansible/playbooks/foo.yml
     -> Will append automatically corpusops related vars based on \$A_ENV_NAME
--
-    '$0' -i foobar .ansible/playbooks/foo.yml
+- '$0' -i foobar .ansible/playbooks/foo.yml
     -> Will execute ansible-playbook as-is
 - NO_SILENT= will make the call run without output unless an error occurs
 - QUIET= will make the call not output the underlying ansible call log
--
-    NO_DEFAULT_VAULTS=1 '$0' .ansible/playbooks/foo.yml
+- HOSTS= vary where the roles will be executed
+- NO_DEFAULT_VAULTS=1 '$0' .ansible/playbooks/foo.yml
     -> Will execute ansible-playbook as-is
 
 '
 }
 
+A_HOSTS=${A_HOSTS:-all}
 parse_cli $@
 if [ -z $TMPPLAYBOOK ];then
     TMPPLAYBOOK=$(mktemp)
@@ -43,7 +42,7 @@ fi
 [[ -z ${ROLES-} ]] && die no roles
 cat > "$TMPPLAYBOOK" << EOF
 ---
-- hosts: [all]
+- hosts: [$ANSIBLE_HOSTS]
   roles:
 EOF
 for i in ${ROLES-};do
