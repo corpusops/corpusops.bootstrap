07cb579cfef1f1691061a8a69c2f779173a96cb0
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2018-04-27 13:30:56 +0200

    usage & output for setupcorpusops

--- hacking/deploy/setup_corpusops.sh
+++ hacking/deploy/setup_corpusops.sh
@@ -1,10 +1,22 @@
 #!/usr/bin/env bash
 set -e
 
+
 COPS_SCRIPTS_DIR="${COPS_SCRIPTS_DIR:-$(cd "$(dirname "$0")" && pwd)}"
 if [ -e "$COPS_SCRIPTS_DIR/ansible_deploy_env" ];then
     . "$COPS_SCRIPTS_DIR/ansible_deploy_env"
 fi
+
+
+usage() {
+    NO_HEADER=y die '
+
+    '$0''"
+
+$(call_cops_installer --help)
+"
+}
+parse_cli $@
 
 if [[ -n ${SKIP_COPS_SETUP-} ]];then die_ 0 "-> Skip corpusops setup";fi
 
@@ -12,13 +24,13 @@ if [[ -n ${SKIP_COPS_SETUP-} ]];then die_ 0 "-> Skip corpusops setup";fi
 if [[ -z "${SKIP_COPS_INSTALL}" ]] \
     && [ ! -e $LOCAL_COPS_ROOT/venv/bin/ansible ];then
     log "Install corpusops"
-    if ! call_cops_installer $COPS_INSTALL_ARGS;then die_ 23 "Install error";fi
+    if ! call_cops_installer $COPS_INSTALL_ARGS $@;then die_ 23 "Install error";fi
 fi
 
 # Update corpusops code, ansible & roles
 if [[ -z "${SKIP_COPS_UPDATE}" ]];then
     log "Refresh corpusops"
-    if ! call_cops_installer  $COPS_UPDATE_ARGS;then die_ 24 "Update error";fi
+    if ! call_cops_installer  $COPS_UPDATE_ARGS $@;then die_ 24 "Update error";fi
 else
     log "-> Skip corpusops update"
 fi
