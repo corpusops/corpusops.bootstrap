39f24dc5488a08608ce5984997d2fda8178f0d7e
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2018-12-02 01:38:05 +0100

    yamldump: Handle files in args

--- hacking/deploy/yamldump.py
+++ hacking/deploy/yamldump.py
@@ -6,14 +6,21 @@ from __future__ import print_function
 import yaml
 import StringIO
 import os
+import sys
 import json
-TEST_FILE = os.environ.get('YAML_TEST_FILE', '.gitlab-ci.yml')
-data = yaml.load(open(TEST_FILE).read())
-edata = json.loads(json.dumps(data))
-out = StringIO.StringIO()
-yaml.safe_dump(edata,
-               out,
-               allow_unicode=True,
-               default_flow_style=False)
-print(out.getvalue())
+files = set()
+for i in sys.argv[1:]:
+    files.add(i)
+if not files:
+    files.add(os.environ.get('YAML_TEST_FILE', '.gitlab-ci.yml'))
+for f in files:
+    print('Dumping {0}\n'.format(f))
+    data = yaml.load(open(f).read())
+    edata = json.loads(json.dumps(data))
+    out = StringIO.StringIO()
+    yaml.safe_dump(edata,
+                   out,
+                   allow_unicode=True,
+                   default_flow_style=False)
+    print(out.getvalue())
 # vim:set et sts=4 ts=4 tw=80:
