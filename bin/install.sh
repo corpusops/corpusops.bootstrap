555992e536f507834c28805504c6738d91a6b222
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2019-02-21 21:18:25 +0100

    Revert "Switch to ansible 2.7"
    This reverts commit def7a708e057667189db10ca2c9eb8ed26aaa21c.

--- bin/install.sh
+++ bin/install.sh
@@ -758,7 +758,7 @@ get_corpusops_branch() {
 }
 
 get_ansible_branch() {
-    get_default_knob ansible_branch "${ANSIBLE_BRANCH}" "stable-2.7"
+    get_default_knob ansible_branch "${ANSIBLE_BRANCH}" "stable-2.4"
 }
 
 set_vars() {
