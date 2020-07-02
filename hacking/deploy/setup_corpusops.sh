28289ef25a3a67ea8346cd7acd7d17952cd48a48
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2020-07-02 10:57:53 +0200

    Be sure to be non interactive

--- hacking/deploy/setup_corpusops.sh
+++ hacking/deploy/setup_corpusops.sh
@@ -24,19 +24,19 @@ if [[ -n ${SKIP_COPS_SETUP-} ]];then die_ 0 "-> Skip corpusops setup";fi
 # calling custom call
 if [[ -n "$@" ]];then
      log "Call corpusops.bootstrap with $@"
-     if ! call_cops_installer $@;then die_ 25 "Install error";fi
+     if ! call_cops_installer -C $@;then die_ 25 "Install error";fi
 else
     # Run install only
     if [[ -z "$@" ]] && [[ -z "${SKIP_COPS_INSTALL}" ]] \
         && [ ! -e $LOCAL_COPS_ROOT/venv/bin/ansible ];then
         log "Install corpusops"
-        if ! call_cops_installer $COPS_INSTALL_ARGS $@;then die_ 23 "Install error";fi
+        if ! call_cops_installer $COPS_INSTALL_ARGS;then die_ 23 "Install error";fi
     fi
 
     # Update corpusops code, ansible & roles
     if [[ -z "$@" ]] &&  [[ -z "${SKIP_COPS_UPDATE}" ]];then
         log "Refresh corpusops"
-        if ! call_cops_installer  $COPS_UPDATE_ARGS $@;then die_ 24 "Update error";fi
+        if ! call_cops_installer $COPS_UPDATE_ARGS;then die_ 24 "Update error";fi
     else
         log "-> Skip corpusops update"
     fi
