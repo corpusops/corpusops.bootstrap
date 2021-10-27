c2cde6c54d4e58a5afec51c29fda6d1f67647cf1
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2021-10-27 21:33:11 +0100

    fix test

--- hacking/deploy.links/download_corpusops.sh
+++ hacking/deploy.links/download_corpusops.sh
@@ -38,8 +38,8 @@ fi
 export COPS_ROOT="${COPS_ROOT:-$DEFAULT_COPS_ROOT}"
 export COPS_URL="${COPS_URL:-https://github.com/corpusops/corpusops.bootstrap.git}"
 export NO_SHARED_COPS="${NO_SHARED_COPS-${SKIP_COPS_FROM_SYSTEM-}}"
-if [[ -n ${SKIP_COPS_SETUP-} ]];then
-    die_ 0 "-> Skip corpusops setup"
+if [[ -n ${SKIP_COPS_DOWNLOAD-} ]];then
+    die_ 0 "-> Skip corpusops download"
 fi
 test_corpusops_present() {
     # in docker images, corpusops has been stripped from some folders
