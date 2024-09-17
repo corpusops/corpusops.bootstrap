596afcf42cb2bc6ceb929493f89bac9f31e2d0be
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2024-09-17 10:56:34 +0300

    Support for ubuntu 24.04 & python 3.12

--- bin/install.sh
+++ bin/install.sh
@@ -43,6 +43,7 @@ SYSTEM_COPS_ROOT=${SYSTEM_COPS_ROOT-$DEFAULT_COPS_ROOT}
 DOCKER_COPS_ROOT=${DOCKER_COPS_ROOT-$SYSTEM_COPS_ROOT}
 COPS_URL=${COPS_URL-$DEFAULT_COPS_URL}
 BASE_PREPROVISION_IMAGES="ubuntu:latest_preprovision"
+BASE_PREPROVISION_IMAGES="$BASE_PREPROVISION_IMAGES corpusops/ubuntu:24.04_preprovision"
 BASE_PREPROVISION_IMAGES="$BASE_PREPROVISION_IMAGES corpusops/ubuntu:22.04_preprovision"
 BASE_PREPROVISION_IMAGES="$BASE_PREPROVISION_IMAGES corpusops/ubuntu:20.04_preprovision"
 BASE_PREPROVISION_IMAGES="$BASE_PREPROVISION_IMAGES corpusops/ubuntu:18.04_preprovision"
@@ -51,6 +52,7 @@ BASE_PREPROVISION_IMAGES="$BASE_PREPROVISION_IMAGES corpusops/ubuntu:14.04_prepr
 BASE_PREPROVISION_IMAGES="$BASE_PREPROVISION_IMAGES corpusops/centos:7_preprovision"
 
 BASE_CORE_IMAGES="$BASE_CORE_IMAGES corpusops/ubuntu:latest"
+BASE_CORE_IMAGES="$BASE_CORE_IMAGES corpusops/ubuntu:24.04"
 BASE_CORE_IMAGES="$BASE_CORE_IMAGES corpusops/ubuntu:22.04"
 BASE_CORE_IMAGES="$BASE_CORE_IMAGES corpusops/ubuntu:20.04"
 BASE_CORE_IMAGES="$BASE_CORE_IMAGES corpusops/ubuntu:18.04"
@@ -62,6 +64,7 @@ EXP_PREPROVISION_IMAGES=""
 EXP_PREPROVISION_IMAGES="$EXP_PREPROVISION_IMAGES archlinux:latest_preprovision"
 EXP_PREPROVISION_IMAGES="$EXP_PREPROVISION_IMAGES debian:latest_preprovision"
 #EXP_PREPROVISION_IMAGES="$EXP_PREPROVISION_IMAGES debian:stretch_preprovision"
+EXP_PREPROVISION_IMAGES="$EXP_PREPROVISION_IMAGES debian:bookworm_preprovision"
 EXP_PREPROVISION_IMAGES="$EXP_PREPROVISION_IMAGES debian:bullseye_preprovision"
 EXP_PREPROVISION_IMAGES="$EXP_PREPROVISION_IMAGES debian:buster_preprovision"
 EXP_PREPROVISION_IMAGES="$EXP_PREPROVISION_IMAGES debian:sid_preprovision"
@@ -71,6 +74,7 @@ EXP_CORE_IMAGES="$EXP_CORE_IMAGES corpusops/debian:latest"
 #EXP_CORE_IMAGES="$EXP_CORE_IMAGES corpusops/debian:stretch"
 EXP_CORE_IMAGES="$EXP_CORE_IMAGES corpusops/debian:bullseye"
 EXP_CORE_IMAGES="$EXP_CORE_IMAGES corpusops/debian:buster"
+EXP_CORE_IMAGES="$EXP_CORE_IMAGES corpusops/debian:bookworm"
 EXP_CORE_IMAGES="$EXP_CORE_IMAGES corpusops/debian:sid"
 EXP_IMAGES="$EXP_PREPROVISION_IMAGES $EXP_CORE_IMAGES"
 # ansible related
@@ -525,8 +529,8 @@ get_python2() {
 get_python3() {
     local py_ver=3
     get_python_ $py_ver \
-        python3.10  python3.9  python3.8  python3.7  python3.6  python3.5  python3.4  \
-        python-3.10 python-3.9 python-3.8 python-3.7 python-3.6 python-3.5 python-3.4 \
+        python3.12  python3.11  python3.10  python3.9  python3.8  python3.7  python3.6  python3.5  python3.4  \
+        python-3.12 python-3.11 python-3.10 python-3.9 python-3.8 python-3.7 python-3.6 python-3.5 python-3.4 \
         python-${py_ver} python${py_ver} python
 }
 has_python_module() {
@@ -545,8 +549,18 @@ pymod_ver() {
 get_setuptools() {
     local py=${1:-python}
     local setuptoolsreq="setuptools"
-    if ( is_python2 $py );then setuptoolsreq="setuptools<=45"; else setuptoolsreq="setuptools<66"; fi
+    local cpyver=$($py -c "import sys;print(sys.version.split()[0])")
+    if ( is_python2 $py );then
+        setuptoolsreq="setuptools<=45"
+    elif ( version_lt $cpyver 3.12.0 );then
+        setuptoolsreq="setuptools<66"
+    else
+        setuptoolsreq="setuptools>=75"
+    fi
     echo "$setuptoolsreq"
+}
+setup_setuptools_requirement() {
+    sed -i -re "s/^setuptools\s*(>|<|=|$)/$(get_setuptools $py)/g" requirements/python_requirements.txt
 }
 install_pip() {
     local py="${1:-python}"
@@ -849,11 +863,11 @@ get_corpusops_roles_branch() {
 }
 
 get_corpusops_branch() {
-    get_default_knob corpusops_branch "${CORPUSOPS_BRANCH}" "3.0"
+    get_default_knob corpusops_branch "${CORPUSOPS_BRANCH}" "4.0"
 }
 
 get_ansible_branch() {
-    get_default_knob ansible_branch "${ANSIBLE_BRANCH}" "stable-2.14"
+    get_default_knob ansible_branch "${ANSIBLE_BRANCH}" "stable-2.17"
 }
 
 move_old_py2_venv() {
@@ -1013,6 +1027,7 @@ import ipwhois
 import pyasn1
 import Crypto.SelfTest.Math
 from distutils.version import LooseVersion
+from distutils.dir_util import remove_tree
 OpenSSL_version = LooseVersion(OpenSSL.__dict__.get('__version__', '0.0'))
 if OpenSSL_version <= LooseVersion('0.15'):
     raise ValueError('trigger upgrade pyopenssl')
@@ -1028,7 +1043,7 @@ recap_(){
     bs_yellow_log "----------------------------------------------------------"
     bs_yellow_log " CORPUSOPS BOOTSTRAP"
     bs_yellow_log "   - ${THIS} [--help] [--long-help]"
-    bs_yellow_log "   version: ${RED}$(get_corpusops_branch)${YELLOW} ansible: ${RED}$(get_ansible_branch)${NORMAL}"
+    bs_yellow_log "   version: ${RED}$(get_corpusops_branch)${YELLOW} ansible: ${RED}$(get_ansible_branch)${YELLOW} roles: ${RED}$(get_corpusops_roles_branch)${NORMAL}"
     if ! corpusops_use_venv;then
         bs_yellow_log "   use venv: ${RED}no${NORMAL}"
     fi
@@ -1066,6 +1081,7 @@ recap_(){
 }
 
 reconfigure() {
+    local py="${1-$(get_cops_python)}"
     for i in ${W}/requirements/*.in;do
         ${SED} -r \
             -e "s#^\# (-e.*__(ANSIBLE))#\1#g" \
@@ -1470,6 +1486,7 @@ install_python_libs_() {
             log "As switching to pycryptodome, we uninstall pycrypto"
             vv $maysudo "$py" -m pip uninstall -y pycrypto
         fi
+        setup_setuptools_requirement
         COPS_UPGRADE="" COPS_PYTHON="$py" ensure_last_python_requirement \
             -r requirements/python_requirements.txt
         die_in_error "requirements/python_requirements.txt doesn't install"
