679a1d98163a57c4e73bac06afb30aceb9920ad1
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2025-09-03 20:00:39 +0300

    backport roles_branch support

--- bin/install.sh
+++ bin/install.sh
@@ -1024,7 +1024,7 @@ recap_(){
     bs_yellow_log "----------------------------------------------------------"
     bs_yellow_log " CORPUSOPS BOOTSTRAP"
     bs_yellow_log "   - ${THIS} [--help] [--long-help]"
-    bs_yellow_log "   version: ${RED}$(get_corpusops_branch)${YELLOW} ansible: ${RED}$(get_ansible_branch)${NORMAL}"
+    bs_yellow_log "   version: ${RED}$(get_corpusops_branch)${YELLOW} ansible: ${RED}$(get_ansible_branch)${YELLOW} roles: ${RED}$(get_corpusops_roles_branch)${NORMAL}"
     if ! corpusops_use_venv;then
         bs_yellow_log "   use venv: ${RED}no${NORMAL}"
     fi
@@ -1569,6 +1569,7 @@ usage() {
     bs_help "    -u|--use-venv yes/no" "do we use venv" "$(get_corpusops_use_venv)" y
     bs_help "    --ansible-url <url>" "ansible fork git url" "$(get_ansible_url)" y
     bs_help "    --ansible-branch <branch>" "ansible fork git branch" "$(get_ansible_branch)" y
+    bs_help "    --roles-branch <branch>" "corpusops ansible roles git branch" "$(get_corpusops_roles_branch)" y
     bs_help "    -C|--no-confirm" "Do not ask for start confirmation" "" y
     bs_help "    --no-colors" "No terminal colors" "${NO_COLORS}" y
     bs_help "    -d|--debug" "activate debug" "${DEBUG}" y
@@ -1671,6 +1672,9 @@ parse_cli_opts() {
         fi
         if [ "x${1}" = "x--ansible-branch" ]; then
             ANSIBLE_BRANCH="${2}";sh="2";argmatch="1"
+        fi
+        if [ "x${1}" = "x--roles-branch" ]; then
+            CORPUSOPS_ROLES_BRANCH="${2}";sh="2";argmatch="1"
         fi
         if [ "x${1}" = "x--upgrade" ]; then
             DO_UPGRADE="1";argmatch="1"
